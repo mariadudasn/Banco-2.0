8b7842c8d6df14253458791afc4ad3c2df748423
mariadudasn <maria.santos081207@gmail.com>
2023-09-26 20:50:31 -0300

    Update

--- Classes.py
+++ Classes.py
@@ -1,63 +1,154 @@
-import os
-
-class Usuário:
+clientes = []
+class Banco:
     def __init__(self):
-        self.cliente = {}
+        self._clientes = []
+        self._nome = ""
+        self._cpf = 0
+        self._idade = 0
+        self._telefone = 0
+        self._email = ""
+        self._senha = ""
+    
+    def adicionar_cliente(self):
+        cliente = {
+            "Conta": len(self._clientes) + 1,
+            "Nome": self._nome,
+            "CPF": self._cpf,
+            "Idade": self._idade,
+            "Telefone": self._telefone,
+            "Email": self._email,
+            "Senha": self._senha,
+            "Saldo": 0
+        }
+        self._clientes.append(cliente)
 
-    def cadastrarCliente (self, nome, email, senha):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-        self.cliente[email] = {'Nome': nome, 'Senha': senha}
-
-    def loginCliente (self, nome, email, senha):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-
-    def verificar_usuario(self, email, senha):
-        if email in self.cliente and self.cliente[email]['Senha'] == senha:
-            return "ACESSO LIBERADO"
-        else:
-            return "ACESSO NEGADO. Digite os dados novamente ou faça seu cadastro."
-
-
-
-class Produtos:
-    def __init__(self, nome2, valor):
-        self.nome2 = nome2
-        self.valor = valor
+    def setNome (self, nome):
+        self._nome = nome
 
     def getNome (self):
-        return self.nome2
-    
-    def getValor (self):
-        return self.valor
-    
-class Produtos_Loja:
-    lista_loja = []
+        return self._nome 
 
-    def cadastrar_produto(self, nome2, valor):
-        produto = Produtos(nome2, valor)
-        self.lista_loja.append (produto)
-        return "PRODUTO ADICIONADO"
+    def setTelefone (self, telefone):
+        self._telefone = telefone
+    
+    def getTelefone (self):
+        return self._telefone 
 
-    def listar_produtos(self):
-        self.cont = 0
-        for x in self.lista_loja:
-            self.cont += 1
-            print (f"{self.cont} - Nome: {x.getNome()} - Valor: {x.getValor()}")
+    def setCPF(self, cpf):
+        self._cpf = cpf
+    
+    def getCPF(self):
+        return self._cpf 
+
+    def setSenha(self, senha):
+        self._senha = senha
+    
+    def setEmail(self, email):
+        self._email = email
+    
+    def getEmail(self):
+        return self._email
+    
+    def setIdade(self, idade):
+        self._idade = idade
+    
+    def getIdade(self):
+        return self._idade
+
+    def setEndereço(self, endereço):
+        self.endereço = endereço
+    
+    def getEndereço(self):
+        return self.endereço
+
         
-class Carrinho(Produtos_Loja):
-    lista_carrinho = []
+    def validar_cliente_por_cpf_e_senha(self, cpf, senha):
+        for cliente in self._clientes:
+            if cliente["CPF"] == cpf and cliente["Senha"] == senha:
+                return cliente
+        return None
+        
+    def excluir_conta(self):
+        del self
+        print("Cliente excluído com sucesso.")
 
-    def adicionar_produto(self):
-        produto_carrinho = self.cont
-        self.lista_carrinho.append (produto_carrinho)
-        return "PRODUTO ADICIONADO AO CARRINHO"
 
-    def visualizar_carrinho(self):
-        pass
+    
+class Cliente:
+    def __init__(self):
+        self._valor = 0
+        self._cpf = 0
+        self._senha = ""
+        self._saldo = 0
+        self._cliente_b = ""
+        self._cliente_a = ""
 
-    def excluir_produto(self):
-        pass
+    def SetValor(self, valor):
+        self._valor = valor
+
+    def GetValor(self):
+        return self._valor
+       
+    def SetCpf(self, cpf):
+        self._cpf = cpf
+
+    def GetCpf(self):
+        return self._cpf
+
+    def SetSenha(self, senha):
+        self._senha = senha
+
+    def GetSenha(self):
+        return self._senha
+
+    def SetSaldo(self, saldo):
+        self._saldo = saldo
+
+    def GetSaldo(self):
+        return self._saldo
+
+    def SetCliente_a(self, cliente_a):
+        self._cliente_a = cliente_a
+
+    def GetCliente_a(self):
+        return self._cliente_a
+
+    def SetCliente_a(self, cliente_b):
+        self._cliente_b = cliente_b
+
+    def GetCliente_b(self):
+        return self._cliente_b
+ 
+
+    def sacar(self, valor):
+        if(self.__if_saque(valor)):
+            self.__saldo -= valor
+
+        else:
+            print(f'o valor de {valor} passou o limite para saque')
+
+
+    def depositar (self, valor, nome, cpf, saldo):
+        if valor > 0:
+            self.saldo += valor
+            self.nome = nome
+            self.cpf = cpf
+            print(f"Depósito de R${valor:.2f} realizado. Novo saldo: R${self.saldo:.2f}") 
+
+    def transferencia (self,valor,cliente_a, cliente_b,cpf, senha,saldo):
+        cliente_a.saldo = cliente_a.depositar()
+        cliente_b.saldo = cliente_b.depositar()
+            
+        cliente_a = Banco.validar_cliente_por_cpf_e_senha()
+
+        if cpf and senha in clientes:
+            cliente_b = int (input("Informe o CPF do destinatário",cpf))
+            for cpf, in clientes:
+                valor = float (input("Informe a quantia que deseja transferir:"))
+                cliente_b.saldo = cliente_b.saldo + valor
+                cliente_a.saldo = cliente_a.saldo - valor
+                print ("Transferencia no valor de R$",valor, "concluída")
+            else:
+                print ("Usuário não encontrado")
+        else:
+            print ("Usuário não encontrado")
---
+++ README.md
@@ -0,0 +1,12 @@
+# Projeto_OO
+Software bancário
+
+Sobre: ...
+
+Requisitos mínimos:
+
+- Cadastrar cliente;
+- excluir cliente;
+- alterar cliente;
+- tranferência entre contas de clientes;
+- deposito e saque de dinheiro.
